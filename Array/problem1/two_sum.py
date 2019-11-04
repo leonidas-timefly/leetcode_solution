4685014ac41f4f560b3b098642d4b0ace44478eb
Tortoise-Dolphin <2442344049@qq.com>
2019-11-05 01:16:33 +0800

    The first time commit code

---
+++ Array/problem1/two_sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(List)):
+            for j in range(i+1, len(List)):
+                if List[i] + List[j] == target
+                    return [List[i],List[j]]
