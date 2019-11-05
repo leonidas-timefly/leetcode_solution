9a1d5afe781ac20eb18ea7752beb673df8a8b18e
Tortoise-Dolphin <2442344049@qq.com>
2019-11-05 09:13:45 +0800

    leetcode problem1:two sum(class:array)

---
+++ Array/problem1/readme.txt
@@ -0,0 +1,6 @@
+Input parameters:
+    self:class of python has parameter self to quickly identify your function
+    nums:the input parameter, it's a list
+    target:the target sum-up
+    ->list[int]:return a list
+In the first solution, I used two "for-cycles"
--- Array/problem1/two_sum.py
+++ Array/problem1/two_sum.py
@@ -1,6 +1,17 @@
+'''
+solution1:Runtime: 5396 ms, faster than 13.71% of Python3 online submissions for Two Sum.
+Memory Usage: 14.9 MB, less than 10.46% of Python3 online submissions for Two Sum.
+Next challenges:
+'''
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(List)):
-            for j in range(i+1, len(List)):
-                if List[i] + List[j] == target
-                    return [List[i],List[j]]
+    def twoSum(self, nums, target: int):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    #print([i, j])
+                    return [i, j]
+
+nums = [2, 7, 11, 15]
+target = 13
+function = Solution()
+function.twoSum(nums, target)
