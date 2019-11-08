be0e2df300ae8800f106cfb784d8d9e2377769ee
Tortoise-Dolphin <2442344049@qq.com>
2019-11-08 23:55:18 +0800

    leetcode problem15:three_sum(class:array)

--- Array/problem15/three_sum.py
+++ Array/problem15/three_sum.py
@@ -4,7 +4,7 @@ Find all unique triplets in the array which gives the sum of zero.
 Note:
 The solution set must not contain duplicate triplets.
 '''
-
+'''
 class Solution:
     def threeSum(self, nums):
           nums.sort()
@@ -33,7 +33,7 @@ class Solution:
                   return []
           else:
               while i < numf and j > m - numz - 1:
-                  if nums[i] + nums[j] == 0 and num0 != 0:
+                  if nums[i] + nums[j] == 0 and num0 != 0 and j > m - numz - 1:
                       out_nums.append([nums[i], 0, nums[j]])
                   if -(nums[i] + nums[j]) < 0:
                       if nums[i] == nums[i+1]:
@@ -57,6 +57,7 @@ class Solution:
                   if nums[i] + nums[j] == 0 and num0 != 0:
                       out_nums.append([nums[i], 0, nums[j]])
                       j -= 1
+                      continue
                   if -(nums[i] + nums[j]) > 0:
                       if nums[j] == nums[j-1]:
                           while nums[j] == nums[j-1]:
@@ -83,8 +84,45 @@ class Solution:
                       i += 1
           print(out_nums)
           return out_nums
+'''
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        lennums = len(nums)
+
+        for i in range(lennums):
+            left = i + 1
+            right = lennums - 1
+
+            if i > 0 and nums[i - 1] == nums[i]:
+                left += 1
+                continue
+
+            while left < right:
+                sumthree = nums[i] + nums[left] + nums[right]
+                if sumthree == 0:
+                    res_col = [nums[i], nums[left], nums[right]]
+                    res.append(res_col)
+                    left += 1
+                    right -= 1
+
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
+
+                if sumthree < 0:
+                    left += 1
+                if sumthree > 0:
+                    right -= 1
+        return res
 
 
-nums1=[-1, 0, 1, 2, -1, -4]
+nums1=[-1, 0, 1, 2, -1, -4, -5, 3]
 function = Solution()
 function.threeSum(nums1)
