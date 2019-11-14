8447364e8479ad9f0b5937e6490e332489a9f4bb
Tortoise-Dolphin <2442344049@qq.com>
2019-11-14 22:49:26 +0800

    leetcode problem42:trapping_rain_water(class:array)

--- Array/problem42/readme.txt
+++ Array/problem42/readme.txt
@@ -2,3 +2,5 @@
 Given n non-negative integers representing an elevation map where the width of each bar is 1,
 compute how much water it is able to trap after raining.
 '''
+index function return only one index of max num
+divide the list by the max num,the water is always determined by the left one
--- Array/problem42/trapping_rain_water.py
+++ Array/problem42/trapping_rain_water.py
@@ -1,7 +1,23 @@
 class Solution:
     def trap(self, height):
-
-
+          if len(height) == 0:
+              return 0
+          max_num_index = height.index(max(height))
+          sum_water=0
+          left_index=0
+          right_index=0
+          for i in range(max_num_index):
+              if left_index <= height[i]:
+                  left_index = height[i]
+              else:
+                  sum_water += left_index - height[i]
+          for i in range(len(height) - 1, max_num_index, -1):
+              if right_index <= height[i]:
+                  right_index = height[i]
+              else:
+                  sum_water += right_index - height[i]
+          print(sum_water)
+          return sum_water
 
 
 
