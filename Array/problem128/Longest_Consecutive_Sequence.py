7e2d1821688e9ab0abfdb80200d4c1c83c235b8c
Tortoise-Dolphin <2442344049@qq.com>
2019-12-16 00:13:48 +0800

    leetcode problem128:longest_consecutive_sequence(array)

--- Array/problem128/Longest_Consecutive_Sequence.py
+++ Array/problem128/Longest_Consecutive_Sequence.py
@@ -5,28 +5,6 @@ Your algorithm should run in O(n) complexity.
 '''
 class Solution:
     def longestConsecutive(self, nums):
-        if len(nums) < 2:
-            return len(nums)
-
-        output = 0
-        tem = 1
-
-        nums = list(set(nums))
-        nums.sort()
-
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1] + 1:
-                tem += 1
-
-            else:
-                if tem > output:
-                    output = tem
-                tem = 1
-
-        if output < tem:
-            output = tem
-
-        return output
         max_len = 0
         nums = list(set(nums))
         nums.sort()
@@ -34,24 +12,19 @@ class Solution:
         if num == 0 or num == 1:
             return num
         num_now = 1
-        #print(nums)
         i = 0
         while i < num - 1:
-            #print(nums[i])
-            #print(nums[i+1])
             while nums[i] == nums[i + 1] - 1:
                 num_now += 1
                 i += 1
                 if i == num - 1:
                     break
-            #print(i)
-            #print(max_len)
-            #print(num_now)
             max_len = max(max_len, num_now)
             num_now = 1
+            i += 1
         print(max_len)
         return max_len
 
-nums1 = [0,0,-1]
+nums1 = [100,4,200,1,3,2]
 function = Solution()
 function.longestConsecutive(nums1)
