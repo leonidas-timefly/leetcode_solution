23ca3d64c6c64e3c1b3f020c3d54e8a372e3f73a
Tortoise-Dolphin <2442344049@qq.com>
2019-12-22 00:55:04 +0800

    leetcode problem169:majority_element(array)

--- Array/problem169/majority_element.py
+++ Array/problem169/majority_element.py
@@ -6,15 +6,30 @@ You may assume that the array is non-empty and the majority element always exist
 '''
 class Solution:
     def majorityElement(self, nums):
+        num = int(len(nums) / 2)
         if len(nums) == 1:
             return nums[0]
         nums.sort()
-        num = [0]*(nums[-1] + 1)
-        for i in range(len(nums)):
-            num[nums[i]] += 1
-            if num[nums[i]] > len(nums)/2:
-                #print(nums[i])
+        for i in range(num + 1):
+            if nums[i] == nums[i + num]:
+                print(nums[i])
                 return nums[i]
+'''
+        now_num = 1
+        for i in range(len(nums) - 1):
+            while nums[i] == nums[i+1]:
+                now_num += 1
+                i += 1
+                if now_num > len(nums) / 2:
+                    #print(nums[i])
+                    return nums[i]
+                if i == len(nums) - 1:
+                    break
+
+            if now_num < len(nums) / 2:
+                now_num = 1
+'''
+
 nums1 = [3,2,3]
 function = Solution()
 function.majorityElement(nums1)
