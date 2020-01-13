00d09a291f81dd2f9b71b779c4c27458f8149056
Tortoise-Dolphin <2442344049@qq.com>
2020-01-13 23:42:03 +0800

    leetcode problem495:teemo_attacking(array)

--- Array/problem495/teemo_attacking.py
+++ Array/problem495/teemo_attacking.py
@@ -10,21 +10,20 @@ class Solution:
     def findPoisonedDuration(self, timeSeries, duration):
         if len(timeSeries) == 0:
             return 0
+        smaller_duration= 0
+        full_duration = 0
         timeSeries.append(timeSeries[-1] + duration)
         for i in range(len(timeSeries) - 1, 0, -1):
             timeSeries[i] = timeSeries[i] - timeSeries[i - 1]
-        #timeSeries.pop(timeSeries[0])
-        #print(timeSeries)
-        smaller_duration= 0
-        full_duration = 0
-        #print(len(timeSeries))
-        for j in range(1, len(timeSeries)):
-            if timeSeries[j] < duration:
-                smaller_duration += timeSeries[j]
+            if timeSeries[i] < duration:
+                smaller_duration += timeSeries[i]
             else:
                 full_duration += 1
-        #print(smaller_duration)
-        #print(full_duration)
+        #for j in range(1, len(timeSeries)):
+            #if timeSeries[j] < duration:
+                #smaller_duration += timeSeries[j]
+            #else:
+                #full_duration += 1
         full_time = smaller_duration + full_duration*duration
         #print(full_time)
         return full_time
