7e3eeaf4581cfdd9a3897fcfeeb933a8cd4ae7e4
Tortoise-Dolphin <2442344049@qq.com>
2020-01-21 22:01:33 +0800

    leetcode problem643:maxmium_average_subarray(unsolved array)

---
+++ Array/problem643/maximum_average_subarray.py
@@ -0,0 +1,10 @@
+'''
+Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average
+value. And you need to output the maximum average value.
+'''
+class Solution:
+    def findMaxAverage(self, nums, k):
+
+nums1 = [1,12,-5,-6,50,3]
+function = Solution()
+function.findMaxAverage(nums1, 3)
