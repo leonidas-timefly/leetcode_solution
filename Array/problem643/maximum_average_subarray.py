822e62d5f6e7980eba79cd822333c6089dfe1af8
Tortoise-Dolphin <2442344049@qq.com>
2020-01-21 23:44:52 +0800

    leetcode problem643:maxmium_average_subarray(unsolved array)

--- Array/problem643/maximum_average_subarray.py
+++ Array/problem643/maximum_average_subarray.py
@@ -4,7 +4,14 @@ value. And you need to output the maximum average value.
 '''
 class Solution:
     def findMaxAverage(self, nums, k):
-
+        max_sum = sum(nums[0:k])
+        sum_windows = max_sum
+        for i in range(k, len(nums)):
+            sum_windows = sum_windows + nums[i] - nums[i -k]
+            max_sum = max(max_sum, sum_windows)
+        #print(head_index)
+        #print(max_sum / k)
+        return max_sum / k
 nums1 = [1,12,-5,-6,50,3]
 function = Solution()
-function.findMaxAverage(nums1, 3)
+function.findMaxAverage(nums1, 4)
