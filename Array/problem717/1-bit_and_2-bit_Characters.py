27c16201fa2aa4a41d9d2fe8d3a9394ca0aba8cd
Tortoise-Dolphin <2442344049@qq.com>
2020-01-30 00:42:41 +0800

    leetcode problem717:1-bit_and_2-bit_Characters(array)

--- Array/problem717/1-bit_and_2-bit_Characters.py
+++ Array/problem717/1-bit_and_2-bit_Characters.py
@@ -7,6 +7,36 @@ The given string will always end with a zero.
 '''
 class Solution:
     def isOneBitCharacter(self, bits):
+        stack = ""
+        for bit in bits:
+            if bit == 1:
+                if stack == "11" or stack == "10":
+                    stack = str(bit)
+                else:
+                    stack += str(bit)
+            if bit == 0:
+                if stack == "1":
+                    stack += str(bit)
+                else:
+                    stack = ""
+        if len(stack) == 0:
+            return True
+        return False
+        stack = ""
+        for b in bits:
+            if b == 1:
+                if stack == "11" or stack == "10":
+                    stack = str(b)
+                else:
+                    stack += str(b)
+            if b == 0:
+                if stack == "1":
+                    stack += str(b)
+                else:
+                    stack = ""
+        if len(stack) == 0:
+            return True
+        return False
 nums1 = [1, 0, 0]
 function = Solution()
 function.isOneBitCharacter(nums1)
