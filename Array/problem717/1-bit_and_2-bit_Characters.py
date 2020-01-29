e1f9729d92de62f4df2d2685a01876ecd95c1fc3
Tortoise-Dolphin <2442344049@qq.com>
2020-01-29 23:57:33 +0800

    leetcode problem717:1-bit_and_2-bit_Characters(array)

---
+++ Array/problem717/1-bit_and_2-bit_Characters.py
@@ -0,0 +1,12 @@
+'''
+We have two special characters. The first character can be represented by one bit 0. The second character can be
+represented by two bits (10 or 11).
+
+Now given a string represented by several bits. Return whether the last character must be a one-bit character or not.
+The given string will always end with a zero.
+'''
+class Solution:
+    def isOneBitCharacter(self, bits):
+nums1 = [1, 0, 0]
+function = Solution()
+function.isOneBitCharacter(nums1)
