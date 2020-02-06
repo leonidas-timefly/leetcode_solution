55ffcae96c26c2dabf380ca516d71816f40aa3c5
Tortoise-Dolphin <2442344049@qq.com>
2020-02-07 00:18:11 +0800

    leetcode problem746:Largest_Number_At_Least_Twice_of _Others(array)

---
+++ Array/problem747/Largest_Number_At_Least_Twice_of_Others.py
@@ -0,0 +1,13 @@
+'''
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+'''
+class Solution:
+    def dominantIndex(self, nums):
+
+nums1 = [3, 6, 1, 0]
+function = Solution()
+function.dominantIndex(3, 3)
