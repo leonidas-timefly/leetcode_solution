e59c819a78203a2f8fddef116ec3d57434a13028
Tortoise-Dolphin <2442344049@qq.com>
2020-02-08 23:45:00 +0800

    leetcode problem766:Toeplitz_Matrix(array)

---
+++ Array/problem766/Toeplitz_Matrix.py
@@ -0,0 +1,11 @@
+'''
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+'''
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+
+nums1 = [3, 6, 1, 0]
+function = Solution()
+function.isToeplitzMatrix(nums1)
