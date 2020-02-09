cffd9bd2a28f12d29d365f2a8d63d59eafdf7490
Tortoise-Dolphin <2442344049@qq.com>
2020-02-09 23:22:55 +0800

    leetcode problem768:Max Chunks_To_Make_Sorted_II(array)

---
+++ Array/problem768/Max_Chunks_To_Make_Sorted_II.py
@@ -0,0 +1,15 @@
+'''
+This question is the same as "Max Chunks to Make Sorted" except the integers of the given array are not necessarily
+distinct, the input array could be up to length 2000, and the elements could be up to 10**8.
+
+Given an array arr of integers (not necessarily distinct), we split the array into some number of "chunks" (partitions)
+, and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+
+What is the most number of chunks we could have made?
+'''
+class Solution:
+    def maxChunksToSorted(self, arr):
+
+nums1 = [5,4,3,2,1]
+function = Solution()
+function.maxChunksToSorted(nums1)
