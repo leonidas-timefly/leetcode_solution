f9749da52070c9f8cfff0a2c7484ddef43b5865e
Tortoise-Dolphin <2442344049@qq.com>
2020-02-11 00:33:52 +0800

    leetcode problem768:Max Chunks_To_Make_Sorted_II(array)

--- Array/problem768/Max_Chunks_To_Make_Sorted_II.py
+++ Array/problem768/Max_Chunks_To_Make_Sorted_II.py
@@ -9,6 +9,20 @@ What is the most number of chunks we could have made?
 '''
 class Solution:
     def maxChunksToSorted(self, arr):
+        temp = []
+        for i in range(len(arr)):
+            temp.append(arr[i])
+        temp.sort()
+        sum_num = 0
+        sum_temp = 0
+        sum_arr = 0
+        for j in range(len(arr)):
+            sum_temp += temp[j]
+            sum_arr += arr[j]
+            if sum_temp == sum_arr:
+                sum_num += 1
+        return sum_num
+'''
         number = 0
         value_first = -1
         index_first = 0
@@ -16,7 +30,7 @@ class Solution:
         while index_later < len(arr) - 1:
             value_first = arr[index_first]
             if value_first > arr[index_later + 1] and arr[index_later] >= arr[index_later + 1] and index_later < len(arr) - 1:
-                #print("+", index_later)
+                print("+", index_later)
                 index_later += 1
             else:
                 number += 1
@@ -26,7 +40,8 @@ class Solution:
             number += 1
         print(number)
         return number
+'''
 
-nums1 = [2,1,3,4,4]
+nums1 = [0,3,0,3,2]
 function = Solution()
 function.maxChunksToSorted(nums1)
