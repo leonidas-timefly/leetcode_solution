d6a94f3a82ab94274164a3ea10ff97bde8379df7
Tortoise-Dolphin <2442344049@qq.com>
2020-02-12 23:34:21 +0800

    leetcode problem766:Toeplitz_matrix(array)

--- Array/problem766/Toeplitz_Matrix.py
+++ Array/problem766/Toeplitz_Matrix.py
@@ -5,7 +5,13 @@ Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
 '''
 class Solution:
     def isToeplitzMatrix(self, matrix):
+        if len(matrix) <= 1 or len(matrix[0]) <= 1:
+            return True
 
-nums1 = [3, 6, 1, 0]
+nums1 = [
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+]
 function = Solution()
 function.isToeplitzMatrix(nums1)
