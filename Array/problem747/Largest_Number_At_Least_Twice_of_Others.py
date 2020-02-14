002b46a07f9402d97dcacf5a4450cdce1ec4bd6f
Tortoise-Dolphin <2442344049@qq.com>
2020-02-14 23:49:43 +0800

    leetcode problem747:largest_number_at_least_twice_of_others(array)

--- Array/problem747/Largest_Number_At_Least_Twice_of_Others.py
+++ Array/problem747/Largest_Number_At_Least_Twice_of_Others.py
@@ -7,8 +7,16 @@ If it is, return the index of the largest element, otherwise return -1.
 '''
 class Solution:
     def dominantIndex(self, nums):
+        max_num = max(nums)
+        part_max_num = max_num / 2
+        for i in range(len(nums)):
+            if nums[i] != max_num and part_max_num < nums[i]:
+                #print("-1")
+                return -1
+        #print("1")
+        return nums.index(max_num)
 
 
-nums1 = [3, 6, 1, 0]
+nums1 = [1, 2, 3, 4]
 function = Solution()
 function.dominantIndex(nums1)
